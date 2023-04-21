fe3ff2ea7f8db6152b80dc778a7627c29432ff0b
Replit user <>
2023-04-21 18:41:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -67,7 +67,7 @@ void enlarge(HashMap * map) {
     map->size = 0;
 
     for(int i = 0 ; i < map->capacity / 2 ; i++)
-        if (aux->buckets[i]->key != NULL)
+        if (aux[i]->key != NULL)
             insertMap(map,aux[i]->key,aux[i]->value);
 
     free(aux);
